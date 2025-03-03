925ca892ee3ddf424b0addd5f87c9efe65f3b42c
namanchandak <98888986+namanchandak@users.noreply.github.com>
2025-03-03 15:46:32 +0530

    controller and routes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
+import  scriptRouter  from "./route/script.route";
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5555;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Script server is on port 8080!');
@@ -10,3 +11,5 @@ app.get('/', (req: Request, res: Response) => {
 app.listen(port, () => {
     console.log(`Script Server running at http://localhost:${port}`);
 });
+
+app.use("/" ,scriptRouter )
